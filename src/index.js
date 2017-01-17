eb769c1f1357e4381bca6aaeb26763efbe4b14f1
John Elliott <johnelliott703@gmail.com>
2017-01-17 00:12:23 -0500

    parse url

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
+// ┌─────────────────────────────────────────────────────────────────────────────┐
+// │                                    href                                     │
+// ├──────────┬┬───────────┬─────────────────┬───────────────────────────┬───────┤
+// │ protocol ││   auth    │      host       │           path            │ hash  │
+// │          ││           ├──────────┬──────┼──────────┬────────────────┤       │
+// │          ││           │ hostname │ port │ pathname │     search     │       │
+// │          ││           │          │      │          ├─┬──────────────┤       │
+// │          ││           │          │      │          │ │    query     │       │
+// "  http:   // user:pass @ host.com : 8080   /p/a/t/h  ?  query=string   #hash "
+// │          ││           │          │      │          │ │              │       │
+// └──────────┴┴───────────┴──────────┴──────┴──────────┴─┴──────────────┴───────┘
+// (all spaces in the "" line should be ignored -- they are purely for formatting)
 import fetch from 'whatwg-fetch'
+import url from 'url'
 
-console.log('about to fetch')
-window.fetch('http swapi.co/api/planets/1/')
-  .then(res => res.text())
-  .then(text => console.log(text))
+const SDK = class SDK {
+  constructor(config) {
+    this.config = config
+    this.baseURl = url.parse(config.url)
+  }
+  getPlanet (id) {
+    const requestURL = url.resolve(this.config.url + '/', `planets/${id}`)
+    console.log('resolved uRL', requestURL)
+    return window.fetch(requestURL, )
+  }
+}
+
+new SDK({
+  url: 'http://swapi.co/api'
+}).getPlanet(1)
+  .then(res => res.json())
+  .then(json => console.log(json))
